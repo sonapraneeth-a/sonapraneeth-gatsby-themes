4f5c6db5dd3710f06d1703f7133c3f92f3f5521c
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-07-16 16:57:24 +0530

    wip(switch): trying to add react switch for common theme

---
+++ packages/common/gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = (themeOptions) => {
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  return {
+    plugins: [
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: "assets",
+          path: `${__dirname}/assets`,
+        },
+      },
+      "gatsby-plugin-emotion",
+      "gatsby-plugin-theme-ui",
+    ],
+  };
+};
--- packages/common/src/layouts/root.js
+++ packages/common/src/layouts/root.js
@@ -1,8 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Switch from "../components/switch";
+import {Global} from "@emotion/core";
+import {useColorMode, css} from "theme-ui";
+
+import sun from "../assets/sun-18.png";
+import moon from "../assets/moon-18.png";
+
+const checkedIcon = (
+  <img
+    alt="Moon indicating dark mode"
+    src={moon}
+    width="18"
+    height="18"
+    role="presentation"
+    style={{
+      pointerEvents: "none",
+      margin: "4.5px 4.5px 4.5px 6.5px",
+    }}
+  />
+);
+
+const uncheckedIcon = (
+  <img
+    alt="Sun indicating light mode"
+    src={sun}
+    width="18"
+    height="18"
+    role="presentation"
+    style={{
+      pointerEvents: "none",
+      margin: "5px 5px 5px 5.5px",
+    }}
+  />
+);
 
 function Root({children}) {
   console.log("Root from common");
+  const [colorMode, setColorMode] = useColorMode();
+  const isDarkTheme = colorMode === "dark";
+  const toggleTheme = (e) => {
+    const updatedTheme = (isDarkTheme ? "light" : "dark");
+    setColorMode(updatedTheme);
+  };
+  const switchColor = "rgb(188, 188, 188)";
   return (
     <div
       style={{
@@ -11,6 +52,54 @@ function Root({children}) {
         width: "90%",
       }}
     >
+      <Global
+        styles={css({
+          "*": {
+            boxSizing: "border-box",
+            lineHeight: "body",
+            fontSize: "18px",
+          },
+          "body": {
+            color: "text",
+            background: "background",
+            margin: 0,
+            fontFamily: "body",
+            boxSizing: "border-box",
+            lineHeight: "body",
+          },
+          "html": {
+            fontSize: "18px",
+          },
+        })}
+      />
+      <Global
+        styles={css({
+          ".react-switch-bg, .react-switch-handle": {
+            borderWidth: "0.5px",
+            borderStyle: "solid",
+            borderColor: `${switchColor}`,
+          },
+        })}
+      />
+      <div
+        style ={{
+          position: "fixed",
+          margin: "19.92px 0",
+          right: "1rem",
+          zIndex: "100",
+        }}
+      >
+        <Switch
+          aria-label="Toggle dark mode"
+          aria-checked={isDarkTheme}
+          onColor="#000"
+          offColor="#f2ee50"
+          checkedIcon={checkedIcon}
+          uncheckedIcon={uncheckedIcon}
+          checked={isDarkTheme}
+          onChange={toggleTheme}
+        />
+      </div>
       {children}
     </div>
   );
