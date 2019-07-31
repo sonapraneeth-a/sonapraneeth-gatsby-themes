a90a3e44b80d2e0226dd95e91a052be58d16b59a
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-07-31 21:01:55 +0530

    feat: updated demo home page
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- demo/base/src/pages/index.js
+++ demo/base/src/pages/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import {RootBulb} from "@sonapraneeth/base";
+import {Header, Container, Main, Footer} from "@sonapraneeth/base";
 
 export default props => (
   <RootBulb>
-    <h1>Hello, world</h1>
+    <Header>
+      <Container>
+        <h1>Hello, world</h1>
+      </Container>
+    </Header>
+    <Main>
+      <Container>
+        <p>Main content</p>
+      </Container>
+    </Main>
+    <Footer>
+      <Container>
+        <p>Copyright. Footer</p>
+      </Container>
+    </Footer>
   </RootBulb>
 );
