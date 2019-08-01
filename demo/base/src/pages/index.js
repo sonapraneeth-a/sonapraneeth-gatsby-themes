673d47e0c38b2963b84e5c3eec4f9b0499c88766
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-01 18:37:57 +0530

    refactor: minor refactoring
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- demo/base/src/pages/index.js
+++ demo/base/src/pages/index.js
@@ -2,22 +2,25 @@ import React from "react";
 import {RootBulb} from "@sonapraneeth/base";
 import {Header, Container, Main, Footer} from "@sonapraneeth/base";
 
-export default props => (
-  <RootBulb>
-    <Header>
-      <Container>
-        <h1>Hello, world</h1>
-      </Container>
-    </Header>
-    <Main>
-      <Container>
-        <p>Main content</p>
-      </Container>
-    </Main>
-    <Footer>
-      <Container>
-        <p>Copyright. Footer</p>
-      </Container>
-    </Footer>
-  </RootBulb>
-);
+export default () => {
+  return (
+    <RootBulb>
+      <Header>
+        <Container>
+          <h1>Hello, world</h1>
+        </Container>
+      </Header>
+      <Main>
+        <Container>
+          <p>Main content</p>
+          <a href="https://theme-ui.com/getting-started">Link</a>
+        </Container>
+      </Main>
+      <Footer>
+        <Container>
+          <p>Copyright. Footer</p>
+        </Container>
+      </Footer>
+    </RootBulb>
+  );
+};
--- packages/base/src/gatsby-plugin-theme-ui/index.js
+++ packages/base/src/gatsby-plugin-theme-ui/index.js
@@ -8,7 +8,7 @@ import fonts from "./fonts";
 import variants from "./variants";
 
 export default {
-  // this enables the color modes feature
+  // This enables the color modes feature
   // and is used as the name for the top-level colors object
   initialColorMode: "light",
   // use CSS custom properties to help avoid flash of colors on
@@ -16,10 +16,17 @@ export default {
   // Refer: https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties
   useCustomProperties: false,
   // Breakpoints supported for the theme
+  // Feature Phone  : 20rem;  // 320px
+  // Mobile         : 40rem;  // 640px
+  // Minitablet     : 48rem;  // 768px
+  // Tablet         : 64rem;  //1024px
+  // Laptop         : 80rem;  //1280px
+  // Desktop        : 86rem;  //1376px
+  // FullHD         : 120rem; //1920px
+  // UltraHD        : 240rem; //3840px
   breakpoints: [
-    "20rem", "40rem", "48rem", "64rem", "80rem",
-    "86rem", "120rem", "240rem",
-  ],
+    20, 40, 48, 64, 80, 86, 120, 240,
+  ].map((item) => { return `${item}rem` }),
   // Available fonts
   ...fonts,
   // Line heights
