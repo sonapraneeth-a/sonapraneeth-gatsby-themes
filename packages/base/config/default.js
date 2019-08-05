1bfc9a2f92b126e7586be5c509bbf968dd6180b1
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-05 17:12:38 +0530

    chore: added extensions for debugging base package
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/base/config/default.js
+++ packages/base/config/default.js
@@ -4,3 +4,5 @@ import Debug from "debug";
 const debugTheme = Debug("@sonapraneeth/base");
 
 export const layout = debugTheme.extend("layout");
+export const component = debugTheme.extend("component");
+export const hooks = debugTheme.extend("hooks");
