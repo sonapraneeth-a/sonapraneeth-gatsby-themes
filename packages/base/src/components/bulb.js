d107ec87772d7795cbd5e5051f660e8dfc08de6a
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-06 22:35:13 +0530

    fix: fixed eslint issues in bulb component
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/base/src/components/bulb.js
+++ packages/base/src/components/bulb.js
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import PropTypes from "prop-types";
 /** @jsx jsx */
 import {jsx} from "theme-ui";
 
 import BulbIcon from "./bulb-icon";
 
-function BulbSwitch({
-  toggleTheme,
-  color = "#ff0",
-  stroke = "#fcee21",
-}) {
+/**
+ *
+ * @param {*} toggleTheme
+ * @param {*} color
+ * @param {*} stroke
+ * @return {JSX}
+ */
+function BulbSwitch({toggleTheme, color = "#ff0", stroke = "#fcee21"}) {
   return (
     <div
       onClick={toggleTheme}
@@ -18,12 +22,20 @@ function BulbSwitch({
         cursor: "pointer",
       }}
     >
-      <BulbIcon
-        color={color}
-        stroke={stroke}
-      />
+      <BulbIcon color={color} stroke={stroke} />
     </div>
   );
 }
 
 export default BulbSwitch;
+
+BulbSwitch.propTypes = {
+  toggleTheme: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  stroke: PropTypes.string,
+};
+
+BulbSwitch.defaultProps = {
+  color: "#ff0",
+  stroke: "#fcee21",
+};
