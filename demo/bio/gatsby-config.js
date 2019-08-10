6faa0403eeffb82eb5a441c8943cad3d4f73a3d4
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-10 18:21:12 +0530

    feat: added demo for the bio theme
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

---
+++ demo/bio/gatsby-config.js
@@ -0,0 +1,32 @@
+const pathPrefix = "";
+const baseUrl = "/";
+let serviceWorkerUrl = pathPrefix + baseUrl;
+serviceWorkerUrl = serviceWorkerUrl.replace(/\/?$/, "/");
+console.log(`Service worker URL: ${serviceWorkerUrl}`);
+
+module.exports = {
+  pathPrefix: pathPrefix,
+  siteMetadata: {
+    appName: "Demo of gatsby-theme-simple-bio",
+    title: "Demo of gatsby-theme-simple-bio",
+    author: "John Doe",
+    siteUrl: "https://gatsby-theme-simple-bio.netlify.com/",
+    description:
+      "This site is a demonstration for using theme " +
+      "gatsby-theme-simple-bio",
+    social: {
+      twitter: "john-doe",
+    },
+  },
+  plugins: [
+    {
+      resolve: "@sonapraneeth/bio",
+      options: {
+        baseUrl: baseUrl,
+        dataPath: "content/data",
+        assetsPath: "content/assets",
+        homePath: "content/home",
+      },
+    },
+  ],
+};
