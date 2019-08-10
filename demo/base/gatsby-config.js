dbc21249413b8da26759f4af6bc0fff3630f4244
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-10 18:40:21 +0530

    fix: fixing issue Unknown field 'siteMetadata' on type 'Site'.
    Fixed the above issue by adding siteMetadata in demo gatsby-config.js
    
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- demo/base/gatsby-config.js
+++ demo/base/gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  siteMetadata: {
+    appName: "Demo of @sonapraneeth/base",
+    title: "Demo of @sonapraneeth/base",
+    author: "John Doe",
+    siteUrl: "https://gatsby-dev-themes-base.netlify.com/",
+    description:
+      "This site is a demonstration for using theme " + "@sonapraneeth/base",
+  },
   plugins: ["@sonapraneeth/base"],
 };
