94a79b479ddd3ae05f4097bc0a482ee0e5cdb2c8
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-11 15:56:17 +0530

    feat: updated styling for card
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/base/src/components/card.js
+++ packages/base/src/components/card.js
@@ -11,7 +11,18 @@ import PropTypes from "prop-types";
  */
 function Card({children, ...props}) {
   return (
-    <div {...props} sx={{bg: "tomato"}}>
+    <div
+      {...props}
+      sx={{
+        bg: "surface",
+        transition: "box-shadow .25s,-webkit-box-shadow .25s",
+        borderRadius: 0,
+        boxShadow:
+          "0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12), " +
+          "0 3px 1px -2px rgba(0,0,0,.2)",
+        width: "100%",
+      }}
+    >
       {children}
     </div>
   );
