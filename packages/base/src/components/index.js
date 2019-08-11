ad8d2671706d9e0a66284c9d0568de9358d81057
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-11 22:43:16 +0530

    feat: added grid and related components
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/base/src/components/index.js
+++ packages/base/src/components/index.js
@@ -1,5 +1,6 @@
 import {Card, CardContent, CardFooter} from "./card/index";
 import {Chip, StatusChip} from "./chip/index";
+import {Grid, GridItem} from "./grid/index";
 import GatsbyImage from "./gatsby-image";
 import HeadMeta from "./head/meta";
 import SEO from "./head/seo";
@@ -10,6 +11,8 @@ export {
   CardFooter,
   Chip,
   StatusChip,
+  Grid,
+  GridItem,
   GatsbyImage,
   HeadMeta,
   SEO,
