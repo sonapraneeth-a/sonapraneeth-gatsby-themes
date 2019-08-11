b3836b9a7dbcd50dbf700aae9e640e05faee2a4a
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-11 22:58:21 +0530

    feat: updated styling to home template in demo-all
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- demo/all/src/@sonapraneeth/bio/templates/home.js
+++ demo/all/src/@sonapraneeth/bio/templates/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import {PageLayout} from "@sonapraneeth/base";
+import {PageLayout, Styled, SContainer} from "@sonapraneeth/base";
 import HomeWidget from "@sonapraneeth/bio/src/widget/home";
 import ProjectWidget from "@sonapraneeth/project/src/widget/projects";
 import BlogWidget from "@sonapraneeth/blog/src/widget/blogs";
@@ -27,9 +27,24 @@ function HomeLayout({location}) {
   const blogs = useBlogs();
   return (
     <PageLayout title={""} description={""} location={location}>
-      <HomeWidget author={home.authorInfo} details={home.mdx} />
-      <ProjectWidget projects={projects} />
-      <BlogWidget blogs={blogs} />
+      <div>
+        <SContainer>
+          <Styled.h1>About</Styled.h1>
+          <HomeWidget author={home.authorInfo} details={home.mdx} />
+        </SContainer>
+      </div>
+      <div>
+        <SContainer>
+          <Styled.h1>Projects</Styled.h1>
+          <ProjectWidget projects={projects} />
+        </SContainer>
+      </div>
+      <div>
+        <SContainer>
+          <Styled.h1>Blog</Styled.h1>
+          <BlogWidget blogs={blogs} />
+        </SContainer>
+      </div>
     </PageLayout>
   );
 }
