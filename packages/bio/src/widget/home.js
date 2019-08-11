d367ae0e39fc9963b6b39d436b3dd13d0fdf7e38
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-11 22:53:50 +0530

    feat: updated styling of home widget
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/bio/src/widget/home.js
+++ packages/bio/src/widget/home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import {Grid, GridItem} from "@sonapraneeth/base";
+
 import FigureBio from "../components/bio/figure";
 import SocialBio from "../components/bio/social";
 import DescriptionBio from "../components/bio/description";
@@ -14,17 +16,31 @@ import DescriptionBio from "../components/bio/description";
 function HomeWidget({author, details}) {
   return (
     <>
-      {author.cover != null && (
-        <FigureBio image={author.cover.childImageSharp} />
-      )}
-      {
-        <SocialBio
-          linkedin={author.username.linkedin}
-          github={author.username.github}
-          twitter={author.username.twitter}
-        />
-      }
-      <DescriptionBio description={details.body} />
+      <Grid noCols={2} nSizes={[1, 2]}>
+        <GridItem
+          id="figureBio"
+          css={{
+            margin: "0 10%",
+            display: "flex",
+            flexDirection: "column",
+            alignSelf: "flex-start",
+          }}
+        >
+          {author.cover != null && (
+            <FigureBio image={author.cover.childImageSharp} />
+          )}
+          {
+            <SocialBio
+              linkedin={author.username.linkedin}
+              github={author.username.github}
+              twitter={author.username.twitter}
+            />
+          }
+        </GridItem>
+        <GridItem id="descriptionBio" css={{padding: "0 1rem 1rem 1rem"}}>
+          <DescriptionBio description={details.body} />
+        </GridItem>
+      </Grid>
     </>
   );
 }
