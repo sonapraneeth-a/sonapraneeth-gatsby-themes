41fbb75de0055ccb07434eca73b0f48b965753f8
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-12 09:55:44 +0530

    fix: updated styling of project template
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/project/src/templates/project.js
+++ packages/project/src/templates/project.js
@@ -3,28 +3,38 @@ import PropTypes from "prop-types";
 import {graphql} from "gatsby";
 import {MDXRenderer} from "gatsby-plugin-mdx";
 
-import {SMain} from "@sonapraneeth/base";
-import {PageLayout} from "@sonapraneeth/base";
+import {SContainer, Styled} from "@sonapraneeth/base";
+import {BaseLayout, Chip} from "@sonapraneeth/base";
+// import TableOfContents from "../components/toc";
 
 /**
  *
  * @param {*} data
  * @return {JSX}
  */
-function Project({data}) {
+function Project({data, location}) {
   console.log(data);
   return (
-    <PageLayout location={data.project.slug} title={data.project.title}>
-      <SMain>
-        {data.project.completed_date}
-        <MDXRenderer>{data.project.body}</MDXRenderer>
-      </SMain>
-    </PageLayout>
+    <BaseLayout location={data.project.slug} title={""}>
+      <SContainer>
+        <Styled.h1>{data.project.title}</Styled.h1>
+        <Chip type={"date"}>{data.project.completed_date}</Chip>
+        <hr />
+        <section>
+          {/* <TableOfContents
+            tableOfContents={data.project.tableOfContents}
+            location={location}
+          />*/}
+          <MDXRenderer>{data.project.body}</MDXRenderer>
+        </section>
+      </SContainer>
+    </BaseLayout>
   );
 }
 
 Project.propTypes = {
   data: PropTypes.any.isRequired,
+  location: PropTypes.any,
 };
 
 Project.defaultProps = {};
@@ -34,6 +44,7 @@ export default Project;
 export const query = graphql`
   query ProjectQuery($id: String!) {
     project(id: { eq: $id }) {
+      title
       status
       start_date
       completed_date
