4bd343c8c98d4f30cafd7167cd05b0a15b209096
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-13 00:47:05 +0530

    feat: added debug option for bio/blog/project packages
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

---
+++ packages/project/config/default.js
@@ -0,0 +1,8 @@
+/* eslint-disable new-cap */
+import Debug from "debug";
+
+const debugTheme = Debug("@sonapraneeth/project");
+
+export const layout = debugTheme.extend("layout");
+export const component = debugTheme.extend("component");
+export const hooks = debugTheme.extend("hooks");
