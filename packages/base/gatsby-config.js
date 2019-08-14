cd88f5d0c1121e3d87c7a71b1d0ecf925f1d5d96
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-14 20:19:29 +0530

    feat: added default siteMetadata for base package to be used in social query hook
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/base/gatsby-config.js
+++ packages/base/gatsby-config.js
@@ -1,5 +1,23 @@
 module.exports = (themeOptions) => {
   return {
+    // Default siteMetadata
+    siteMetadata: {
+      appName: "Base",
+      title: "Base",
+      author: "John Doe",
+      // TODO: Need to change this URL
+      siteUrl: "https://www.gatsbyjs.org",
+      description:
+        "This site is a demonstration for using theme " +
+        "gatsby-theme-simple-bio",
+      social: {
+        facebook: "john-doe",
+        twitter: "john-doe",
+        email: "john-doe",
+        linkedin: "john-doe",
+        github: "john-doe",
+      },
+    },
     plugins: ["gatsby-plugin-theme-ui"],
   };
 };
