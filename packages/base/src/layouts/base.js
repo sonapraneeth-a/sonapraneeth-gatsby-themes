130bd32a790cc47b749c980ee4b1f1ac292be38d
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-18 18:22:41 +0530

    fix: added a missing import in base layout
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/base/src/layouts/base.js
+++ packages/base/src/layouts/base.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import PropTypes from "prop-types";
+import {Link} from "gatsby";
 import {Styled} from "theme-ui";
 /** @jsx jsx */
 import {jsx} from "theme-ui";
