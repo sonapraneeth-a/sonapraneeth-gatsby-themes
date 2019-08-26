ba52c033f8ef5f2e84ca5ff86346ce32d0b4100c
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-26 19:33:06 +0530

    fix: updated styling for button and changed from div to button
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/gatsby-plugin-themed-components/src/components/button.js
+++ packages/gatsby-plugin-themed-components/src/components/button.js
@@ -1,46 +1,55 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import {jsx} from "theme-ui";
 // eslint-disable-next-line no-unused-vars
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 
 /**
  *
  * @param {*} children
  * @return {JSX}
  */
-function Button({ children, type, ...props }) {
-  const style = "button." + type
+function Button({children, type, ...props}) {
+  const style = "button." + type;
   return (
-    <div
+    <button
       {...props}
       sx={{
         display: "inline-block",
         fontWeight: "body",
         textAlign: "center",
         verticalAlign: "middle",
-        px: ".4rem",
-        py: ".25rem",
-        fontSize: 3,
-        lineHeight: "body",
-        fontFamily: "body",
-        WebkitAppearance: "none",
-        MozAppearance: "none",
+        userselect: "none",
+        border: "1px solid transparent",
+        padding: ".375rem .75rem",
+        fontSize: "1rem",
+        borderRadius: ".25rem",
+        cursor: "pointer",
+        WebkitAppearance: "button",
+        overflow: "visible",
+        margin: 0,
+        textDecoration: "none !important",
+        transition:
+          "color .15s ease-in-out, background-color .15s " +
+          "ease-in-out,border-color .15s " +
+          "ease-in-out,box-shadow .15s ease-in-out",
         variant: style,
+        lineHeight: "1.5 !important",
+        fontFamily: "body",
       }}
     >
       {children}
-    </div>
-  )
+    </button>
+  );
 }
 
 Button.propTypes = {
   children: PropTypes.any.isRequired,
   type: PropTypes.oneOf(["primary", "secondary"]),
-}
+};
 
 Button.defaultProps = {
   type: "",
-}
+};
 
-export default Button
+export default Button;
