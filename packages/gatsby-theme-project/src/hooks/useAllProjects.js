18da911015c21034acf0695ea2fa0e0d2515e0ce
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-17 23:25:57 +0530

    fix: added try-catch block for static queries in project package
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/gatsby-theme-project/src/hooks/useAllProjects.js
+++ packages/gatsby-theme-project/src/hooks/useAllProjects.js
@@ -3,23 +3,27 @@ import {useStaticQuery, graphql} from "gatsby";
 // Hook for retrieving site meta data
 export const useAllProjects = () => {
   /* Reference: https://www.gatsbyjs.org/docs/use-static-query/ */
-  const projects = useStaticQuery(
-    graphql`
-      query AllProjectsQuery {
-        allProject {
-          edges {
-            node {
-              id
-              slug
-              title
-              startDate
-              completedDate
-              abstract
+  try {
+    const projects = useStaticQuery(
+      graphql`
+        query AllProjectsQuery {
+          allProject {
+            edges {
+              node {
+                id
+                slug
+                title
+                startDate
+                completedDate
+                abstract
+              }
             }
           }
         }
-      }
-    `
-  );
-  return projects.allProject.edges;
+      `
+    );
+    return projects.allProject.edges;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
--- packages/gatsby-theme-project/src/hooks/useRecentProjects.js
+++ packages/gatsby-theme-project/src/hooks/useRecentProjects.js
@@ -3,24 +3,28 @@ import {useStaticQuery, graphql} from "gatsby";
 // Hook for retrieving site meta data
 export const useRecentProjects = () => {
   /* Reference: https://www.gatsbyjs.org/docs/use-static-query/ */
-  const projects = useStaticQuery(
-    graphql`
-      query RecentProjectsQuery {
-        allProject(limit: 10, sort: { fields: startDate, order: DESC }) {
-          edges {
-            node {
-              id
-              slug
-              title
-              startDate
-              completedDate
-              abstract
-              tags
+  try {
+    const projects = useStaticQuery(
+      graphql`
+        query RecentProjectsQuery {
+          allProject(limit: 10, sort: { fields: startDate, order: DESC }) {
+            edges {
+              node {
+                id
+                slug
+                title
+                startDate
+                completedDate
+                abstract
+                tags
+              }
             }
           }
         }
-      }
-    `
-  );
-  return projects.allProject.edges;
+      `
+    );
+    return projects.allProject.edges;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
