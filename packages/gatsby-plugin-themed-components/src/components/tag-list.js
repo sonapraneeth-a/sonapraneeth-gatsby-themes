5629947b720bc9e62e45fb4ed5514df051f99cef
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-17 23:23:43 +0530

    fix: added margin between tag chips in tag-list component
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/gatsby-plugin-themed-components/src/components/tag-list.js
+++ packages/gatsby-plugin-themed-components/src/components/tag-list.js
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+import {jsx} from "@sonapraneeth/gatsby-plugin-themed-components";
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import PropTypes from "prop-types";
 import {Chip} from "@sonapraneeth/gatsby-plugin-themed-components";
@@ -14,7 +17,13 @@ function TagList({tags}) {
         return (
           tag !== null &&
           tag !== "" && (
-            <Chip type={"tag"} key={"project-tag-" + index}>
+            <Chip
+              type={"tag"}
+              key={"project-tag-" + index}
+              sx={{
+                mx: "0.1rem",
+              }}
+            >
               {tag}
             </Chip>
           )
