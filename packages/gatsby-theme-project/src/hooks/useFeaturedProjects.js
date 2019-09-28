053f3ed34b3647b2f5c998140ae43c222228dc53
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-28 10:12:41 +0530

    fix: added try catch to static query hook in use featured projects
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/gatsby-theme-project/src/hooks/useFeaturedProjects.js
+++ packages/gatsby-theme-project/src/hooks/useFeaturedProjects.js
@@ -3,27 +3,32 @@ import {useStaticQuery, graphql} from "gatsby";
 // Hook for retrieving site meta data
 export const useFeaturedProjects = () => {
   /* Reference: https://www.gatsbyjs.org/docs/use-static-query/ */
-  const projects = useStaticQuery(
-    graphql`
-      query FeaturedProjectsQuery {
-        allProject(
-          sort: { fields: startDate, order: DESC }
-          filter: { featured: { eq: true } }
-        ) {
-          edges {
-            node {
-              id
-              slug
-              title
-              startDate
-              completedDate
-              abstract
-              tags
+  try {
+    const projects = useStaticQuery(
+      graphql`
+        query FeaturedProjectsQuery {
+          allProject(
+            sort: { fields: startDate, order: DESC }
+            filter: { featured: { eq: true } }
+            limit: 10
+          ) {
+            edges {
+              node {
+                id
+                slug
+                title
+                startDate
+                completedDate
+                abstract
+                tags
+              }
             }
           }
         }
-      }
-    `
-  );
-  return projects.allProject.edges;
+      `
+    );
+    return projects.allProject.edges;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
