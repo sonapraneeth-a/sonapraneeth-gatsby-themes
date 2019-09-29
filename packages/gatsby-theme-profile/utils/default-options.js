a6cbada1f8223368f3f6538dabf4634a90dcf46c
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-29 20:09:23 +0530

    fix: fixed issue in options of profile package
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/gatsby-theme-profile/utils/default-options.js
+++ packages/gatsby-theme-profile/utils/default-options.js
@@ -3,15 +3,15 @@ module.exports = (themeOptions) => {
   const author = themeOptions.author || "";
   const baseUrl = {
     home: themeOptions.baseUrl.home || "/",
-    projects: themeOptions.baseUrl.home || "/projects",
-    blog: themeOptions.baseUrl.home || "/blog",
+    projects: themeOptions.baseUrl.projects || "/projects",
+    blog: themeOptions.baseUrl.blog || "/blog",
   };
   const paths = {
     data: themeOptions.paths.data || "content/data",
-    assets: themeOptions.paths.data || "content/assets",
-    home: themeOptions.paths.data || "content/home",
-    projects: themeOptions.paths.data || "content/projects",
-    blog: themeOptions.paths.data || "content/blog",
+    assets: themeOptions.paths.assets || "content/assets",
+    home: themeOptions.paths.home || "content/home",
+    projects: themeOptions.paths.projects || "content/projects",
+    blog: themeOptions.paths.blog || "content/blog",
   };
   // Configure MDX. true would defaults of the theme
   // Default: true
