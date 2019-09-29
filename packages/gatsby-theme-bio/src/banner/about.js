473e900b97ac64acaa53d742a859a5490017d42d
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-09-29 20:37:29 +0530

    fix: updated about banner component to take author data as input
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/gatsby-theme-bio/src/banner/about.js
+++ packages/gatsby-theme-bio/src/banner/about.js
@@ -2,14 +2,17 @@
 import {jsx} from "@sonapraneeth/gatsby-plugin-themed-components";
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import PropTypes from "prop-types";
 
 import {
   Styled,
   SContainer,
 } from "@sonapraneeth/gatsby-plugin-themed-components";
-import {HomeWidget, useHome} from "@sonapraneeth/gatsby-theme-bio";
+import {HomeWidget} from "@sonapraneeth/gatsby-theme-bio";
 
-AboutBanner.propTypes = {};
+AboutBanner.propTypes = {
+  data: PropTypes.any.isRequired,
+};
 
 AboutBanner.defaultProps = {};
 
@@ -17,8 +20,7 @@ AboutBanner.defaultProps = {};
  * Home layout - Used in home page
  * @return {JSX} Rendered children for the AboutBanner
  */
-function AboutBanner() {
-  const home = useHome();
+function AboutBanner({data}) {
   return (
     <div
       sx={{
@@ -28,7 +30,7 @@ function AboutBanner() {
     >
       <SContainer>
         <Styled.h1>About</Styled.h1>
-        <HomeWidget author={home.authorInfo} details={home.mdx} />
+        <HomeWidget author={data.authorInfo} details={data.mdx} />
       </SContainer>
     </div>
   );
