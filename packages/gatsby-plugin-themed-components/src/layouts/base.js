18d57910712e2a03b2417fde041200cedf89fb5f
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-11-11 10:12:25 +0530

    feat(layout): updated root and base layout to use sidebar and navbar
    Sidebar and Navbar would be visible based on window dimensions
    
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- packages/gatsby-plugin-themed-components/src/layouts/base.js
+++ packages/gatsby-plugin-themed-components/src/layouts/base.js
@@ -9,12 +9,24 @@ import HeadMetadata from "../components/head/meta";
 import {SMain} from "../components/theme-ui/index";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import {useMenu} from "../hooks/useMenu";
+import Navbar from "../components/menu/navbar";
+import {useWindowDimensions} from "../hooks/index";
+import ScrollToTop from "../components/scroll-to-top";
 
 /**
  * Base layout for all other layouts
  * @return {JSX} Rendered children for the page
  */
 function BaseLayout({title, description, location, children}) {
+  console.log(location);
+  const menu = useMenu();
+  const {width} = useWindowDimensions();
+  console.log(width);
+  let showNavbar = false;
+  if (width >= 1024) {
+    showNavbar = true;
+  }
   return (
     <Root>
       <HeadMetadata
@@ -22,7 +34,9 @@ function BaseLayout({title, description, location, children}) {
         description={description}
         location={location}
       />
+      <ScrollToTop />
       <Header />
+      {showNavbar === true && <Navbar menuItems={menu} />}
       <SMain>{children}</SMain>
       <Footer />
     </Root>
--- packages/gatsby-plugin-themed-components/src/layouts/root-bulb.js
+++ packages/gatsby-plugin-themed-components/src/layouts/root-bulb.js
@@ -1,4 +1,7 @@
-import React from "react";
+/** @jsx jsx */
+import {jsx} from "theme-ui";
+// eslint-disable-next-line no-unused-vars
+import React, {useContext, useState} from "react";
 import PropTypes from "prop-types";
 import {Global} from "@emotion/core";
 import {css} from "theme-ui";
@@ -6,25 +9,47 @@ import {useColorMode, useThemeUI, Layout} from "theme-ui";
 
 import {layout} from "../../utils/debug";
 import BulbSwitch from "../components/bulb";
+import Sidebar from "../components/menu/sidebar";
+import {useMenu} from "../hooks/useMenu";
+import {SidebarContext} from "../context/sidebar";
+import BreakpointChip from "../components/chip/breakpoint";
 
 /**
  * @param {*} children
  * @return {JSX}
  */
-function RootBulb({children}) {
+function RootBulb({children, windowWidth, windowHeight}) {
   if (typeof window !== "undefined") {
     // If environment is development, attach debug package
     if (process.env.NODE_ENV === "development") {
       // To enable debugging information in browser
       localStorage.setItem(
         "debug",
-        "@sonapraneeth/gatsby-plugin-themed-components:*"
+        "@sonapraneeth/gatsby-plugin-themed-components:*",
       );
     }
   }
   layout("RootBulb from base");
   const [colorMode, setColorMode] = useColorMode();
   const modes = ["light", "dark"];
+  const {type, display, width, unit} = useContext(SidebarContext);
+  /* let type = "push";
+  if (windowWidth >= 1024) {
+    type = "fixed";
+  }*/
+  let hideSidebar = false;
+  if (windowWidth >= 1024) {
+    hideSidebar = true;
+  }
+  const [sidebarDisplay, setSidebarDisplay] = useState(display);
+  if (typeof window !== undefined) {
+    localStorage.setItem("sidebar", type);
+  }
+  const toggleSidebarDisplay = () => {
+    layout("Toggling sidebar");
+    layout(`Sidebar display: ${sidebarDisplay}`);
+    setSidebarDisplay(!sidebarDisplay);
+  };
   // Reference: https://theme-ui.com/recipes/color-mode-switcher
   const toggleTheme = (e) => {
     const index = modes.indexOf(colorMode);
@@ -34,6 +59,31 @@ function RootBulb({children}) {
   const context = useThemeUI();
   layout(`Theme: ${JSON.stringify(context.theme, null, 2)}`);
   const {colors} = context.theme;
+  const menu = useMenu();
+  // eslint-disable-next-line max-len
+  const isMenuPresent = menu !== undefined && menu !== null && menu.length !== 0;
+  const windowWidthInRem = windowWidth / 18;
+  let additionalToggleContentCSS = {};
+  if (isMenuPresent) {
+    additionalToggleContentCSS = {
+      ...additionalToggleContentCSS,
+      transform: sidebarDisplay === true ? `translateX(${width}${unit})` : "",
+      position: type === "push" ? "" : "relative",
+      left: type === "push" ? "0rem" : `${width}${unit}`,
+      // Note: 0.5vw is to be subtracted to accomodate scrollbar
+      /* width:
+        type === "push" ?
+          ((sidebarDisplay === true) ? `${100-width-0.1}${unit}` : "99.5vw") :
+          `${100-width-0.4}${unit}`,*/
+      width:
+        type === "push" ?
+          sidebarDisplay === true ?
+            `${windowWidthInRem - width}${unit}` :
+            "" :
+          `${windowWidthInRem - width}${unit}`,
+    };
+  }
+  console.log("Root");
   return (
     <>
       <Global
@@ -47,6 +97,7 @@ function RootBulb({children}) {
             fontFamily: "body",
             boxSizing: "border-box",
             lineHeight: "body",
+            transition: "all 0.3s ease-in-out",
           },
           "html": {
             fontSize: "18px",
@@ -93,7 +144,31 @@ function RootBulb({children}) {
       >
         <BulbSwitch toggleTheme={toggleTheme} color={colors.bulb.color} />
       </div>
-      <Layout>{children}</Layout>
+      <SidebarContext.Provider
+        value={{
+          type: type,
+          display: sidebarDisplay,
+          width: width,
+          unit: unit,
+        }}
+      >
+        {hideSidebar === false && isMenuPresent === true && (
+          <Sidebar
+            menuItems={menu}
+            toggleSidebarDisplay={toggleSidebarDisplay}
+          />
+        )}
+        <Layout
+          sx={{
+            transition: "all 0.3s ease-in-out",
+            marginTop: [8, 8, 8, 0],
+            ...additionalToggleContentCSS,
+          }}
+        >
+          {children}
+        </Layout>
+        <BreakpointChip text={`${windowWidth} x ${windowHeight}`} />
+      </SidebarContext.Provider>
     </>
   );
 }
@@ -102,6 +177,8 @@ export default RootBulb;
 
 RootBulb.propTypes = {
   children: PropTypes.any.isRequired,
+  windowWidth: PropTypes.number.isRequired,
+  windowHeight: PropTypes.number.isRequired,
 };
 
 RootBulb.defaultProps = {};
