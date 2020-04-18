cae23b10c982a1198e4590a0f3f7145056ab7184
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2020-04-18 13:14:16 +0530

    chore: added script to update packages

---
+++ update-packages.bat
@@ -0,0 +1,52 @@
+call ncu -u --timeout 10000000
+call pushd packages\gatsby-plugin-themed-components
+call ncu -u --timeout 10000000
+call popd
+call pushd packages\gatsby-theme-bio
+call ncu -u --timeout 10000000
+call popd
+call pushd packages\gatsby-theme-blog
+call ncu -u --timeout 10000000
+call popd
+call pushd packages\gatsby-theme-collection
+call ncu -u --timeout 10000000
+call popd
+call pushd packages\gatsby-theme-profile
+call ncu -u --timeout 10000000
+call popd
+call pushd packages\gatsby-theme-project
+call ncu -u --timeout 10000000
+call popd
+call pushd packages\gatsby-theme-publication
+call ncu -u --timeout 10000000
+call popd
+call pushd packages\gatsby-theme-timeline
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\all
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\base
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\bio
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\blog
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\collection
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\profile
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\project
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\publication
+call ncu -u --timeout 10000000
+call popd
+call pushd demo\timeline
+call ncu -u --timeout 10000000
+call popd
